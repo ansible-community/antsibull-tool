327f4ea2b9fc3077b5be59d9c1c70e16afb51ee2
Felix Fontein <felix@fontein.de>
2024-09-27 22:27:18 +0300

    Use pydantic for CollectionDetails. (#6)

--- tests/units/test_collection.py
+++ tests/units/test_collection.py
@@ -10,47 +10,6 @@ import pytest
 
 from antsibull_tool.collection import CollectionDetails, load_collection_details
 
-DATA_GOOD = [
-    (
-        {"namespace": "foo", "name": "bar"},
-        CollectionDetails(namespace="foo", name="bar", dependencies={}),
-    ),
-    (
-        {"namespace": "foo", "name": "bar", "dependencies": {"foo": "bar"}},
-        CollectionDetails(namespace="foo", name="bar", dependencies={"foo": "bar"}),
-    ),
-]
-
-
-@pytest.mark.parametrize("data, expected", DATA_GOOD)
-def test_collection_details_load(data, expected):
-    assert CollectionDetails.load(data) == expected
-
-
-DATA_BAD = [
-    ({"namespace": 42}, "namespace is not a string, but <class 'int'>"),
-    ({"namespace": ""}, "name is not a string, but <class 'NoneType'>"),
-    (
-        {"namespace": "foo", "name": "bar", "dependencies": []},
-        "dependencies is not a mapping, but <class 'list'>",
-    ),
-    (
-        {"namespace": "foo", "name": "bar", "dependencies": {42: "value"}},
-        "dependencies key 42 is not a string",
-    ),
-    (
-        {"namespace": "foo", "name": "bar", "dependencies": {"key": 23}},
-        "dependencies.key value 23 is not a string",
-    ),
-]
-
-
-@pytest.mark.parametrize("data, expected", DATA_BAD)
-def test_collection_details_load_failure(data, expected):
-    with pytest.raises(ValueError, match=f"^{re.escape(expected)}$"):
-        CollectionDetails.load(data)
-
-
 LOAD_DATA_GOOD = [
     (
         r"""---
@@ -110,19 +69,19 @@ LOAD_DATA_BAD = [
 - name: bar
 """,
         "galaxy.yml",
-        "galaxy.yml is not a global mapping",
+        "galaxy\\.yml is not a global mapping$",
     ),
     (
         r"""---
 namespace: foo
 """,
         "galaxy.yml",
-        "name is not a string, but <class 'NoneType'>",
+        "1 validation error for CollectionDetails\nname\n  Field required.*",
     ),
     (
         r"""{"collection_info": []}""",
         "MANIFEST.json",
-        "Cannot find collection_info in MANIFEST.json",
+        "Cannot find collection_info in MANIFEST\\.json$",
     ),
 ]
 
@@ -132,7 +91,7 @@ def test_load_collection_details_failure(data, filename, expected, tmp_path):
     (tmp_path / filename).write_text(data, encoding="utf-8")
     with pytest.raises(
         ValueError,
-        match=f"^Error while loading collection details from .*: {re.escape(expected)}$",
+        match=f"^Error while loading collection details from .*: {expected}",
     ):
         load_collection_details(tmp_path)
 
